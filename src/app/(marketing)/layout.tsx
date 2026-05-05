eac40a1e8212af4687a5eb0c306638affd9aacfc
Nomad <67918840+mursal14@users.noreply.github.com>
2026-05-05 09:34:43 -0500

    Delete src directory

--- src/app/(marketing)/layout.tsx
+++
@@ -1,4 +0,0 @@
-// Route group layout - no metadata here (defined in root layout.tsx)
-export default function MarketingLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
-}
